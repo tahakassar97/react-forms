c3843168dfe2d4e78cedf8fde65fceda8cd62983
Taha Kassar <tahakassar54@gmail.com>
2021-05-30 14:02:53 +0300

    30/5

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { FormHelperText } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-	formControl: {
-		margin: theme.spacing(1),
-		minWidth: 150,
-	},
-}));
+
 
 export default function DropDown({ element, elements, handleChange, inputLabel, helperText, width }) {
-	const classes = useStyles();
 	const data = elements.map((element) => {
 		return <MenuItem value={element.id}>{element.title}</MenuItem>;
 	});
